b6bd0bbf246d27dfe901536be1bc910a8452ee25
sergiocastelarfernandez <sergio.castelarf@gmail.com>
2022-07-28 09:08:44 -0500

    Algebra y geometría con Python

--- read_and_print_matrix_from_console.py
+++ read_and_print_matrix_from_console.py
@@ -1,21 +1,24 @@
 def read_matrix_from_console():
-    n = int(input())  # Number of rows
-    m = int(input())  # Number of columns
+    n = int(input("Number of rows: "))  # Number of rows
+    m = int(input("Number of columns: "))  # Number of columns
     A = []
 
+    print(f"\nThe matrix will be of size {n} x {m}\n")
+
     for i in range(n):
-        row = input().split()
+        row = input(f"Type the {m} elements of the row #{i + 1} separated by a space: ").split()
         for j in range(m):
             row[j] = int(row[j])
-            A.append(row)
+        A.append(row)
     return A
 
 
 def print_matrix_to_console(A):
+    print()
     for row in A:
         for element in row:
-            print(element, end="")
-            print()
+            print(element, end=" ")
+        print()
 
 
 def run():
@@ -23,5 +26,5 @@ def run():
     print_matrix_to_console(A)
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     run()
