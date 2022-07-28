90ad694eb600b0372a34cb7cd2d448b8069dbf51
sergiocastelarfernandez <sergio.castelarf@gmail.com>
2022-07-28 17:33:52 -0500

    Operaciones básicas con matrices Basic operations with matrices

--- standard_operations.py
+++ standard_operations.py
@@ -5,10 +5,28 @@
 
 def matrix_add(A, B):
     if len(A) == len(B) and len(A[0]) == len(B[0]):
-        C = [[0 for j in range(len(A[0]))] for i in range(len(A))]
+        C = [[0 for j in range(len(A[0]))] for i in range(
+            len(A))]  # Create a null matrix C, with the same dimension as matrices A and B | Crea un matriz nula C, con la misma dimensión que las matrices A y B
 
         for i in range(len(A)):
             for j in range(len(A[0])):
                 C[i][j] = A[i][j] + B[i][j]
 
         return C
+
+
+def matrix_mult_by_scalar(A, alpha):
+    C = [[0 for j in range(len(A[0]))] for i in range(len(A))]
+
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            C[i][j] = alpha * A[i][j]
+    return C
+
+
+def run():
+    pass
+
+
+if __name__ == "__main":
+    run()
